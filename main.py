e9648bc01dfc66cac81643ae294c911a07d10e1d
ambiti0n <fabulousfresh010@gmail.com>
2024-01-09 06:33:15 +0000

    bot brain build

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 
 from openfabric_pysdk.context import Ray, State
 from openfabric_pysdk.loader import ConfigClass
+from brain import pred_class, get_response, words,classes,data
 
 
 ############################################################
@@ -25,8 +26,8 @@ def execute(request: SimpleText, ray: Ray, state: State) -> SimpleText:
     output = []
     for text in request.text:
         # TODO Add code here
-        response = 'Hello!'
-        print('got here')
+        intents = pred_class(text, words, classes)
+        response = get_response(intents, data)
         output.append(response)
 
       
